9a57e7af66091b0ed0f6a147371bf18a7d89327b
PriyankaPK26 <118288268+PriyankaPK26@users.noreply.github.com>
2024-02-28 16:54:17 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
-// your code here
+if (num <= 0) {
+        return "Input should be a positive integer";
+    } else if (num === 1) {
+        return 0;
+    } else if (num === 2) {
+        return 1;
+    } else {
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    }
 }
 
 module.exports = fibonacci;
